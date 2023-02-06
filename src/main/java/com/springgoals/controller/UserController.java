4c9b673f78ed5f32969d65ca1ccdd70f152935a7
angjela.milosavljevi <angelanike@live.com>
2023-02-06 14:18:57 +0100

    changes

--- src/main/java/com/springgoals/controller/UserController.java
+++ src/main/java/com/springgoals/controller/UserController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RequestMapping("/api/user")
 public class UserController {
 
-
     private UserServiceImpl userService = new UserServiceImpl();
 
     private static List<User> users;
